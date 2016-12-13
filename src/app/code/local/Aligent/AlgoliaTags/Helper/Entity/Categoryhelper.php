32f2697761fd43ea30e976c369ff0a52ee883120
Michael Wylde <michael.wylde@aligent.com.au>
2016-12-13 15:56:49 +1030

    Trim and remove empty tags. Add attribute to collection query.

--- src/app/code/local/Aligent/AlgoliaTags/Helper/Entity/Categoryhelper.php
+++ src/app/code/local/Aligent/AlgoliaTags/Helper/Entity/Categoryhelper.php
@@ -3,9 +3,10 @@
 class Aligent_AlgoliaTags_Helper_Entity_Categoryhelper extends Algolia_Algoliasearch_Helper_Entity_Categoryhelper
 {
     protected $tagAttribute = null;
-    protected $mapTagAttribute = false;
+    protected $mapTagAttribute = null;
 
-    protected function getTagAttributeMapping() {
+    protected function getTagAttributeMapping()
+    {
         if ($this->tagAttribute === null) {
             $this->tagAttribute = Mage::getStoreConfig('algoliasearch/categories/tag_attribute_mapping');
 
@@ -16,16 +17,34 @@ class Aligent_AlgoliaTags_Helper_Entity_Categoryhelper extends Algolia_Algoliase
         return $this->tagAttribute;
     }
 
+    protected function shouldMapTagAttribute()
+    {
+        if ($this->mapTagAttribute === null) {
+            $this->getTagAttributeMapping();
+        }
+        return $this->mapTagAttribute;
+    }
+
     public function getObject(Mage_Catalog_Model_Category $category)
     {
         $data = parent::getObject($category);
 
-        $tagAttribute = $this->getTagAttributeMapping();
-
-        if ($this->mapTagAttribute) {
-            $tags = explode(',', $category->getData($tagAttribute));
-            $data['_tags'] = array_merge($data['_tags'], $tags);
+        if ($this->shouldMapTagAttribute()) {
+            //explode list, trim, remove empty, merge with original _tags attribute
+            $data['_tags'] = array_merge($data['_tags'], array_filter(array_map('trim', explode(',', $category->getData($this->getTagAttributeMapping())))));
         }
         return $data;
+    }
+
+    public function getCategoryCollectionQuery($storeId, $categoryIds = null)
+    {
+        $categories = parent::getCategoryCollectionQuery($storeId, $categoryIds);
+
+        // Add tag attribute to collectionQuery
+        if ($this->shouldMapTagAttribute()) {
+            $categories->addAttributeToSelect($this->getTagAttributeMapping());
+        }
+
+        return $categories;
     }
 }
